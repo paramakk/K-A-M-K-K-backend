d35a2a6d222b3c854105e00eae706995643c0a90
Mark Banu <mark.banu@clarmont.hu>
2020-03-06 10:12:34 +0100

    initial commit, spring boot application  created

---
+++ src/main/java/projekt33/kamkk/KamkkBackendApplication.java
@@ -0,0 +1,13 @@
+package projekt33.kamkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamkkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamkkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projekt33/kamkk/KamkkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt33.kamkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamkkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
