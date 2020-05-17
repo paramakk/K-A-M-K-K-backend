a729e304a1c8ec8351b173d3a0bfd9f326db7bab
Marton Baradics <marton.badarics@clarmont.hu>
2020-05-17 21:26:11 +0200

    added saveAllCards

--- src/main/java/projekt33/kamkk/service/CardGroupService.java
+++ src/main/java/projekt33/kamkk/service/CardGroupService.java
@@ -1,6 +1,12 @@
 package projekt33.kamkk.service;
 
+import projekt33.kamkk.entity.dto.CardDTO;
 import projekt33.kamkk.entity.dto.CardGroupDTO;
 import projekt33.kamkk.service.base.CrudService;
 
-public interface CardGroupService extends CrudService<Long, CardGroupDTO> {}
+import java.util.List;
+
+public interface CardGroupService extends CrudService<Long, CardGroupDTO> {
+
+    CardGroupDTO saveAllCards(Long id, List<CardDTO> cardDTOS);
+}
