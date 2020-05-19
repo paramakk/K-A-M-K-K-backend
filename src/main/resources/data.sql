e57360fcc36b8e185f6c85b627b094a2c8de2ad1
Mark Banu <mark.banu@clarmont.hu>
2020-05-19 14:48:12 +0200

    sample data created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+-- secret: 'c2NyZXQ=' ( secret )
+--
+-- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.category VALUES (1, 'c2NyZXQ=', 'Matematika');
+INSERT INTO public.category VALUES (2, 'c2NyZXQ=', 'Informatika');
+INSERT INTO public.category VALUES (3, 'c2NyZXQ=', 'Kémia');
+INSERT INTO public.category VALUES (4, 'c2NyZXQ=', 'Biológia');
+INSERT INTO public.category VALUES (5, 'c2NyZXQ=', 'Fizika');
+INSERT INTO public.category VALUES (6, 'c2NyZXQ=', 'Közgazdaságtan');
+INSERT INTO public.category VALUES (7, 'c2NyZXQ=', 'Jogtudomány');
+
+--
+-- Data for Name: theme; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.theme VALUES (1, NULL, 'Analízis 1', 1);
+INSERT INTO public.theme VALUES (2, NULL, 'Analízis 2', 1);
+INSERT INTO public.theme VALUES (3, NULL, 'Analízis 3', 1);
+INSERT INTO public.theme VALUES (4, NULL, 'Analízis 4', 1);
+INSERT INTO public.theme VALUES (5, NULL, 'Diszkrét matematika 1', 1);
+INSERT INTO public.theme VALUES (6, NULL, 'Diszkrét matematika 2', 1);
+INSERT INTO public.theme VALUES (7, NULL, 'Numerikus módszerek 1', 1);
+INSERT INTO public.theme VALUES (8, NULL, 'Numerikus módszerek 2', 1);
+INSERT INTO public.theme VALUES (9, NULL, 'Programozási alapismeretek', 2);
+INSERT INTO public.theme VALUES (10, NULL, 'Programozás', 2);
+INSERT INTO public.theme VALUES (11, NULL, 'Funkcionális programozás', 2);
+INSERT INTO public.theme VALUES (12, NULL, 'Műszaki kémia', 3);
+INSERT INTO public.theme VALUES (13, NULL, 'Szerves kémia', 3);
+INSERT INTO public.theme VALUES (14, NULL, 'Általános kémia', 3);
+INSERT INTO public.theme VALUES (15, NULL, 'Rendszertan', 4);
+INSERT INTO public.theme VALUES (16, NULL, 'Szervezet', 4);
+INSERT INTO public.theme VALUES (17, NULL, 'Klasszikus fizika', 5);
+INSERT INTO public.theme VALUES (18, NULL, 'Modern fizika', 5);
+INSERT INTO public.theme VALUES (19, NULL, 'Alapismeretek', 6);
+INSERT INTO public.theme VALUES (20, NULL, 'Intézményi közgazdaságtan', 6);
+INSERT INTO public.theme VALUES (21, NULL, 'Alternatív közgazdaságtan', 6);
+INSERT INTO public.theme VALUES (22, NULL, 'Büntetőjog 1', 7);
+INSERT INTO public.theme VALUES (23, NULL, 'Büntetőjog 2', 7);
+INSERT INTO public.theme VALUES (24, NULL, 'Latin', 7);
